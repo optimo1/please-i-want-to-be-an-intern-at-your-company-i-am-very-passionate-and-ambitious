ff08b5f5561f8de3eb7d38174747ee586525bc0a
adilzhan.turaliev018 <adilzhan.turaliev018@gmail.com>
2025-11-16 13:43:40 +0000

    3346356

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Counter from "@/components/Counter";
 
 const Index = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background p-4">
+    <div className="flex min-h-screen w-full items-center justify-center bg-background p-4 sm:p-6 md:p-8">
       <Counter />
     </div>
   );
